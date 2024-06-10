669c9f47ccc749691d7e6225d1ac8e20d5702913
Thinkpad <dima.tima22@gmail.com>
2024-06-10 11:51:36 +0300

    создал pojo - class cat, в классе AppConfig создал бин с именем cat.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = applicationContext.getBean("helloworld",HelloWorld.class);
+
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean.equals(bean1) );
+        System.out.println(catBean1.equals(catBean2) );
     }
 }
